9e1b19db91209b6449af48ce07b0359968e5b1f2
Nishant Singh <nishantsingh@Nishants-MacBook-Pro.local>
2023-07-20 17:23:14 +0530

    Bugs resolved and tested

--- android/src/main/java/com/hawk/plugin/rollbar/CapacitorRollbarPlugin.java
+++ android/src/main/java/com/hawk/plugin/rollbar/CapacitorRollbarPlugin.java
@@ -1,50 +1,20 @@
 package com.hawk.plugin.rollbar;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.rollbar.android.Rollbar;
 
 
 @CapacitorPlugin(name = "CapacitorRollbar")
 public class CapacitorRollbarPlugin extends Plugin {
 
-    @PluginMethod
-    public void updateAndroidManifest(PluginCall call) {
-        String accessToken = call.getString("accessToken");
+    @Override
+    public void load() {
+        super.load();
 
-        if (accessToken != null) {
-            updateMetaInManifest(accessToken);
-
-            JSObject result = new JSObject();
-            result.put("message", "Meta-data added to AndroidManifest.xml successfully.");
-            call.success(result);
-        } else {
-            call.reject("Invalid access token provided.");
-        }
-    }
-
-    private void updateMetaInManifest(String accessToken) {
-        try {
-            // Update the AndroidManifest.xml with the provided meta-data
-            getBridge()
-                .getActivity()
-                .runOnUiThread(
-                    () -> {
-                        try {
-                            String packageName = getBridge().getContext().getPackageName();
-                            getBridge()
-                                .getContext()
-                                .getApplicationInfo()
-                                .metaData.putString(packageName + ".com.rollbar.android.ACCESS_TOKEN", accessToken);
-                        } catch (Exception e) {
-                            Log.e("MyPlugin", "Error updating AndroidManifest.xml: " + e.getMessage());
-                        }
-                    }
-                );
-        } catch (Exception e) {
-            Log.e("MyPlugin", "Error updating AndroidManifest.xml: " + e.getMessage());
-        }
+        // Retrieve the access token from capacitor.config.json
+        String accessToken = getConfig().getString("accessToken");
+        String environment = getConfig().getString("environment");
+        Rollbar.init(getContext(), accessToken, "spectro_android", true);
     }
 }
