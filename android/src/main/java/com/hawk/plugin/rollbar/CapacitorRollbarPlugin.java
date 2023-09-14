776a1dcf36297856f605c66e38c68a50afa7d1b6
Nishant Singh <nishantsingh@Nishants-MacBook-Pro.local>
2023-09-14 11:50:19 +0530

    setPersonData added for rollbar and deviceBuildSerialNo sent to rollbar by default

--- android/src/main/java/com/hawk/plugin/rollbar/CapacitorRollbarPlugin.java
+++ android/src/main/java/com/hawk/plugin/rollbar/CapacitorRollbarPlugin.java
@@ -15,6 +15,25 @@ public class CapacitorRollbarPlugin extends Plugin {
         // Retrieve the access token from capacitor.config.json
         String accessToken = getConfig().getString("accessToken");
         String environment = getConfig().getString("environment");
-        Rollbar.init(getContext(), accessToken, "spectro_android", true);
+        Rollbar.init(getContext(), accessToken, environment, true);
+        String deviceBuildSerialNo = android.os.Build.SERIAL;
+        Rollbar.setPersonData(null, null, null, deviceBuildSerialNo);
     }
+
+
+    @PluginMethod
+    public void setPersonData(PluginCall call) {
+        JSObject userData = call.getObject("userData");
+        if (userData != null) {
+            String userId = userData.getString("userId");
+            String userEmail = userData.getString("userEmail");
+            String userName = userData.getString("userName");
+            String deviceBuildSerialNo = android.os.Build.SERIAL;
+            Rollbar.setPersonData(userId, userEmail, userName, deviceBuildSerialNo);
+            call.resolve();
+        }else{
+            call.reject("Invalid user data provided.")
+        }
+    }
+
 }
